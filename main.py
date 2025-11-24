d9ad225e58bb5e2140dccbc2d843190fd8142fa8
harshita94694 <152490408+harshita94694@users.noreply.github.com>
2025-11-24 15:59:04 +0530

    Updated FastAPI code

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "FastAPI Online!"}
+    return {"message": "FastAPI Running on Railway!"}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
